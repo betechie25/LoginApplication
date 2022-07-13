632b7d9d2602dc946b914d7f12f4d971925fd451
Prerna Gupta <prerna.gupta@irissoftware.com>
2022-07-13 13:40:53 +0530

    Static assets used

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.port || 3000;
 
 app.set('view-engine','ejs');
 
+//use static asset
+app.use('/static',express.static(path.join(__dirname,'public')))
+app.use('/asset',express.static(path.join(__dirname,'assets')))
+
 //home route
 app.get('/',(req,res)=>{
-    res.render('base.ejs')
+    res.render('base.ejs',{title:'Login Application'})
 })
 
 
